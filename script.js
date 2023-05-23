0d8ebc7ecc1dc2e2e53d5d1bd8eaf2e8e3fb5fec
krishstud <132578982+krishstud@users.noreply.github.com>
2023-05-23 21:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
 function saveData() {
-    var inputElement = document.getElementById("input-data");
-    var inputData = inputElement.value;
-  
-    if (inputData) {
-      var savedData = localStorage.getItem("myData");
-      if (savedData) {
-        savedData = JSON.parse(savedData);
-        savedData.push(inputData);
-      } else {
-        savedData = [inputData];
-      }
-  
+  var inputElement = document.getElementById("input-data");
+  var inputData = inputElement.value;
+
+  if (inputData) {
+    var savedData = localStorage.getItem("myData");
+    if (savedData) {
+      savedData = JSON.parse(savedData);
+      savedData.push(inputData);
+    } else {
+      savedData = [inputData];
+    }
+
+    localStorage.setItem("myData", JSON.stringify(savedData));
+    inputElement.value = "";
+
+    displaySavedData();
+  }
+}
+
+function deleteData(index) {
+  var savedData = localStorage.getItem("myData");
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+
+    if (index >= 0 && index < savedData.length) {
+      savedData.splice(index, 1);
       localStorage.setItem("myData", JSON.stringify(savedData));
-      inputElement.value = "";
-  
       displaySavedData();
     }
   }
-  
-  function displaySavedData() {
-    var savedData = localStorage.getItem("myData");
-    var savedDataList = document.getElementById("saved-data-list");
-    savedDataList.innerHTML = "";
-  
-    if (savedData) {
-      savedData = JSON.parse(savedData);
-  
-      savedData.forEach(function(data) {
-        var listItem = document.createElement("li");
-        listItem.textContent = data;
-        savedDataList.appendChild(listItem);
-      });
-    }
+}
+
+function displaySavedData() {
+  var savedData = localStorage.getItem("myData");
+  var savedDataList = document.getElementById("saved-data-list");
+  savedDataList.innerHTML = "";
+
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+
+    savedData.forEach(function(data, index) {
+      var listItem = document.createElement("li");
+      listItem.textContent = data;
+
+      var deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.className = "delete-button";
+      deleteButton.onclick = function() {
+        deleteData(index);
+      };
+
+      listItem.appendChild(deleteButton);
+      savedDataList.appendChild(listItem);
+    });
   }
-  
-  displaySavedData();
-  
+}
+
+displaySavedData();
